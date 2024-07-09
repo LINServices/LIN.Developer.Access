3878f0411c68468615223cb142c7fc4ecd941365
Alex <giraldojhong4@gmail.com>
2024-07-09 16:10:49 -0500

    New URL

--- Build.cs
+++ Build.cs
@@ -8,7 +8,7 @@ public class Build
     public static void Init(string? url = null)
     {
         Service._Service = new();
-        Service._Service.SetDefault(url ?? "http://api.cloud.services.linapps.co/");
+        Service._Service.SetDefault(url ?? "https://api.cloud.services.linplatform.com/");
         //Service._Service.SetDefault("http://localhost:5089/");
     }
 
