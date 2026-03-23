340cb7cd3101495a54853554eb33f64423856145
Alex Giraldo <giraldojhong4@gmail.com>
2026-03-23 02:48:43 -0500

    Mejoras de update

---
+++ Controllers/Notification.cs
@@ -0,0 +1,38 @@
+﻿namespace LIN.Access.Developer.Controllers;
+
+public static class Notifications
+{
+    public static async Task<ReadAllResponse<NotificationModel>> ReadAllAsync(string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("notifications");
+
+        // Headers.
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Get<ReadAllResponse<NotificationModel>>();
+
+        // Retornar.
+        return Content;
+
+    }
+    public static async Task<ResponseBase> MarkRead(string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("notifications");
+
+        // Headers.
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Delete<ResponseBase>();
+
+        // Retornar.
+        return Content;
+
+    }
+
+}
